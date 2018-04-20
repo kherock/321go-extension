50faf809f61399d2b66371edc6c5c3df716e0390
Kyle Herock <kherock@gmail.com>
2018-04-20 18:35:46 -0400

    refactor(popup): Use a BehaviorSubject for reacting to room ID changes
    This also finally fixes all the ESLint errors from using element IDs as global variables.

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -6,109 +6,117 @@ import 'chrome-extension-async';
 import mdcAutoInit from '@material/auto-init';
 import { MDCRipple } from '@material/ripple';
 import { MDCTextField } from '@material/textfield';
+import { BehaviorSubject } from 'rxjs';
 
-mdcAutoInit.register('MDCRipple', MDCRipple);
-mdcAutoInit.register('MDCTextField', MDCTextField);
-
+for (const component of [MDCRipple, MDCTextField]) {
+  mdcAutoInit.register(component.name, component);
+}
 mdcAutoInit();
 
-const port = chrome.runtime.connect();
+const {
+  createRoomBtn,
+  joinRoomForm,
+  leaveRoomBtn,
+  requestPermissionBtn,
+  roomView,
+  popupView,
+} = window;
 
 const roomIdControl = joinRoomForm.roomId.parentElement.MDCTextField;
 const submitBtn = joinRoomForm.submitBtn;
 
-async function ensurePermissions(url) {
-  const hasPermission = await chrome.permissions.contains({ origins: [url] });
-  if (!hasPermission) {
-    const granted = await chrome.permissions.request({ origins: [url] });
-    if (!granted) throw new Error('Permission denied');
+const port = chrome.runtime.connect();
+
+class Popup {
+  constructor(tab, roomId) {
+    this.tab = tab;
+    this.roomId = new BehaviorSubject(roomId);
   }
-}
 
-function initView(tab, roomId) {
-  if (roomId) {
-    roomIdControl.value = roomId;
-    roomIdControl.layout();
-    updateRoomView(roomId, tab);
+  initView() {
+    this.roomId.subscribe((value) => {
+      if (value) {
+        roomIdControl.value = value;
+        roomIdControl.layout();
+      }
+      createRoomBtn.disabled = !!value;
+      roomIdControl.disabled = !!value;
+      submitBtn.disabled = !!value || !roomIdControl.valid || !roomIdControl.value;
+      this.updateRoomView();
+      this.setRoomView();
+    });
+
+    joinRoomForm.roomId.addEventListener('input', (ev) => {
+      submitBtn.disabled = !roomIdControl.value || !roomIdControl.valid;
+    });
+    createRoomBtn.addEventListener('click', async (ev) => {
+      if (createRoomBtn.disabled) return;
+      createRoomBtn.disabled = true;
+      submitBtn.disabled = true;
+      const hasPermission = await chrome.permissions.request({ origins: [this.tab.url] });
+      if (!hasPermission) return;
+      port.postMessage({
+        tab: this.tab.id,
+        type: 'CREATE_ROOM',
+      });
+    });
+    joinRoomForm.addEventListener('submit', async (ev) => {
+      ev.preventDefault();
+      createRoomBtn.disabled = true;
+      roomIdControl.disabled = true;
+      submitBtn.disabled = true;
+      port.postMessage({
+        tab: this.tab.id,
+        type: 'JOIN_ROOM',
+        roomId: roomIdControl.value,
+      });
+    });
+    requestPermissionBtn.addEventListener('click', async (ev) => {
+      const hasPermission = await chrome.permissions.request({ origins: [this.tab.url] });
+      if (!hasPermission) return;
+      roomView.classList.remove('error');
+      roomView.classList.remove('permission-error');
+      port.postMessage({
+        tab: this.tab.id,
+        type: 'RESYNC_MEDIA',
+      });
+    });
+    leaveRoomBtn.addEventListener('click', (ev) => {
+      port.postMessage({
+        tab: this.tab.id,
+        type: 'LEAVE_ROOM',
+      });
+    });
+
+    port.onMessage.addListener(async (message) => {
+      switch (message.type) {
+      case 'JOIN_ROOM':
+        this.roomId.next(message.roomId);
+        break;
+      case 'LEAVE_ROOM':
+        this.roomId.next(null);
+        break;
+      case 'PERMISSION_REQUIRED':
+        roomView.classList.add('error');
+        roomView.classList.add('permission-error');
+        this.tab = await chrome.tabs.get(this.tab.id);
+        this.updateRoomView();
+        break;
+      }
+    });
   }
-  toggleRoomView(!!roomId);
 
-  joinRoomForm.roomId.addEventListener('input', (ev) => {
-    submitBtn.disabled = !roomIdControl.value || !roomIdControl.valid;
-  });
-  createRoomBtn.addEventListener('click', async (ev) => {
-    if (createRoomBtn.disabled) return;
-    createRoomBtn.disabled = true;
-    await ensurePermissions(tab.url);
-    port.postMessage({
-      tab: tab.id,
-      type: 'CREATE_ROOM',
-    });
-  });
-  joinRoomForm.addEventListener('submit', async (ev) => {
-    ev.preventDefault();
-    createRoomBtn.disabled = true;
-    roomIdControl.disabled = true;
-    submitBtn.disabled = true;
-    port.postMessage({
-      tab: tab.id,
-      type: 'JOIN_ROOM',
-      roomId: roomIdControl.value,
-    });
-  });
-  requestPermissionBtn.addEventListener('click', async (ev) => {
-    await ensurePermissions(tab.url);
-    roomView.classList.remove('error');
-    roomView.classList.remove('permission-error');
-    port.postMessage({
-      tab: tab.id,
-      type: 'RESYNC_MEDIA',
-    });
-  });
-  leaveRoomBtn.addEventListener('click', (ev) => {
-    port.postMessage({
-      tab: tab.id,
-      type: 'LEAVE_ROOM',
-    });
-  });
-
-  port.onMessage.addListener(async (message) => {
-    switch (message.type) {
-    case 'JOIN_ROOM':
-      roomIdControl.value = message.roomId;
-      roomIdControl.layout();
-      updateRoomView(message.roomId);
-      toggleRoomView(true);
-      break;
-    case 'LEAVE_ROOM':
-      toggleRoomView(false);
-      break;
-    case 'PERMISSION_REQUIRED':
-      roomView.classList.add('error');
-      roomView.classList.add('permission-error');
-      updateRoomView(roomIdControl.value, await chrome.tabs.get(tab.id));
-      break;
-    }
-  });
-}
-
-async function updateRoomView(roomId, tab) {
-  roomView.querySelector('.room-id-text').textContent = roomId;
-  if (tab) {
-    const hasPermission = await chrome.permissions.contains({ origins: [tab.url] });
+  async updateRoomView() {
+    roomView.querySelector('.room-id-text').textContent = this.roomId.value;
+    const hasPermission = await chrome.permissions.contains({ origins: [this.tab.url] });
     if (!hasPermission) {
       roomView.classList.add('error');
       roomView.classList.add('permission-error');
     }
   }
-}
 
-function toggleRoomView(force = !popupView.classList.contains('in-room')) {
-  popupView.classList.toggle('in-room', force);
-  if (!force) {
-    createRoomBtn.disabled = false;
-    roomIdControl.disabled = false;
-    submitBtn.disabled = !roomIdControl.value || !roomIdControl.valid;
+  setRoomView() {
+    popupView.classList.toggle('in-room', !!this.roomId.value);
   }
 }
 
@@ -116,6 +124,6 @@ chrome.tabs.query({ active: true, currentWindow: true }).then(([tab]) => {
   if (!tab) return;
   port.onMessage.addListener(function initListener(rooms) {
     port.onMessage.removeListener(initListener);
-    initView(tab, rooms[tab.id]);
+    new Popup(tab, rooms[tab.id]).initView();
   });
 });
